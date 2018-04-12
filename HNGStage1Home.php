0c00045b7db45d588205c4965c0454392dce735d
rock-zion <rock_zion@outlook.com>
2018-04-12 14:20:58 +0100

    Add files via upload

---
+++ HNGStage1Home.php
@@ -0,0 +1,83 @@
+ <!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<head>
+<title>HNG Internship 4.0 Home Page</title>
+<link rel="stylesheet" type="text/css" href="HNG Stage 1 Home Style.css">
+</head>
+<body>
+<div class = "topnav">
+    <a href="#home">
+        <img src="HNG Internship.png" alt="HNG logo" height="50" width="443.17">
+    </a>
+    
+</div>
+<div width="100%">
+        <div class="learn">
+                <p>Learn the best way possible,</p>
+                <p>hands on, from the biggest</p>
+                <p>remote internship in africa.</p>
+                <div class="benefits"> 
+                    <ul>
+                        <li>
+                                Learn real world coding
+                        </li>
+                        <li>
+                                Get paid when you complete tasks
+                        </li>
+                    </ul>
+                </div>
+                <div class="datetime">
+                        <?php
+                        echo "   " .  date("d/m/Y") . "<br>";
+                        echo date("h:i:sa");
+                        ?> 
+                </div>
+                <button onclick='window.open("bit.ly/2GSqBJs")' type="button">GET STARTED</button>
+        
+        </div>
+        <div class="image">
+                <img src="569 [Converted].png" alt="" height="675px" width="675px">
+        </div>
+</div>
+
+<div class="Partners" width="100%">
+        <p>
+                Our Partners
+        </p>
+        <div>
+                <img src="1.png" alt="" >
+                <img src="2.png" alt="" >
+                <img src="3.png" alt="" >
+                <img src="4.png" alt="" >
+        </div>
+</div>
+<div class="Graduates">
+        <p>
+               Graduates
+        </p>
+<div class="Gallery">
+        <div class="boy1" >
+                <img src="boy.png" alt="HNG logo" height="128" width="128">
+                <div class="name">John Doe</div>
+        </div>
+        <div class="girl" >
+               <img src="girl.png" alt="HNG logo" height="128" width="128">
+                <div class="name">Jane Doe</div>
+        </div>
+        <div class="boy2" >
+                <img src="boy.png" alt="HNG logo" height="128" width="128">
+                <div class="name">David Doe</div>
+        </div>
+</div>
+
+</div>
+<div class="footer" width="100%">
+        <div class="Twitter" >
+                <div>Follow Us</div>
+                <img src="unnamed (4).png" alt="" width="55" height="48">
+        </div>
+</div>
+
+</body>
+</html> 
